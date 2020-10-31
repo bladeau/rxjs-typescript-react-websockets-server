f58db73c8655a2fd66dd7d9bf8caad240906002a
Al <al.almaktoum@live.com>
2020-10-31 16:31:29 +1100

    Created a namespace "live" and in that a room called "stocks". When client connects broadcast to the room

--- stock-server.js
+++ stock-server.js
@@ -4,17 +4,22 @@ const io = require('socket.io')(http);
 const randomStockObject = require('./random-stock-values');
 
 const port = process.env.PORT || 9090;
-
+io.of('/live'); //create namespaces
 io.on('connection', (socket) => {
-  getStockValues(socket);
-  setInterval(() => {
-    getStockValues(socket);
-  }, 5000);
+  socket.on('joinStocksRoom', (room) => {
+    if (room == 'stocks') {
+      socket.join('stocks');
+      setInterval(() => {
+        io.of('/live').to('stocks').emit('liveStocks'),
+          [randomStockObject.getAppleStockValues()];
+      }, 5000);
+    }
+  });
 });
 
-function getStockValues(socket) {
-  socket.emit('appleStocks', randomStockObject.getAppleStockValues());
-}
+// function getStockValues(socket) {
+//   socket.emit('appleStocks', randomStockObject.getAppleStockValues());
+// }
 
 http.listen(port, () =>
   console.log(`The stock server is istening on PORT - ${port}`)
